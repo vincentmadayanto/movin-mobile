d5f742c3b960ee979aed50b7fbff50fa4a349b57
Vincent Madayanto <vincentmadayanto@gmail.com>
2024-12-09 15:19:54 +0700

    [ADD] add constant for imported images

---
+++ src/constant/images.js
@@ -0,0 +1,4 @@
+export const headerImage =
+  "https://res.cloudinary.com/dwtj0kyiy/image/upload/v1733729494/header-image_uqz1ne.png";
+export const emptyImage =
+  "https://res.cloudinary.com/dwtj0kyiy/image/upload/v1733729754/empty_ge46mr.png";
